b21a0ac0f1b718bed0920324dee23dd57a77a585
NoSignificantWorries <dmitry.karpachev@hotmail.com>
2025-02-21 18:20:47 +0700

    parser updated

--- parser/main.py
+++ parser/main.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
 
 
 class WildberriesCSS:
